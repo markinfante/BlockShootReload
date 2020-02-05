4bada2d2fda770da9474bf31aa170ed9f65e6d83
markinfante <minfante@vassar.edu>
2020-02-05 18:00:32 -0500

    Added function to take user input. Main method now asks for difficulty level.

--- BlockShootReload.java
+++ BlockShootReload.java
@@ -16,10 +16,44 @@ class BlockShootReload {
 
     }
 
-    public static void main(String[] args){
+    void printDifficulty(){
 
+        System.out.println("Would you like to play on easy, medium or difficult?");
+
+    }
+
+    public String getUserString(){
+
+        String output;
+        // open up readers to take user input and convert to string
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            output = br.readLine().toLowerCase().trim();
+            return output;
+        }
+        catch (IOException io){
+            System.out.print(io);
+            return "oops.";
+        }
+
+    }
+
+    public static void main(String[] args){
+        // If given any arguments print help message and terminate.
+        if (args.length > 1) {
+            System.out.println("BlockShootReload takes no arguments.");
+            System.exit(1);
+        }
         BlockShootReload newGame = new BlockShootReload();
         newGame.printIntro();
+        newGame.printDifficulty();
+        if (newGame.getUserString().equals("easy")){
+            System.out.println("Okay. Starting game on easy.");
+
+        } else {
+            System.out.println("Only easy is implemented. Starting game on easy");
+        }
 
     }
 }
